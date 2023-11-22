c4132c6a0ac5db538ddd03f02b7ae8a301fcd87e
bigbugboy <liuxu-ynl@fixmail.com>
2023-11-22 15:16:03 +0800

    expenses paginator

--- expenses/views.py
+++ expenses/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 from .models import Category, Expense
 
 
 @login_required(login_url='login')
 def index(request):
+    page = request.GET.get('page', 1)
+    expenses = Expense.objects.all()
+    paginator = Paginator(expenses, 5)
     context = {
-        'expenses': Expense.objects.all()
+        'page_obj': paginator.get_page(page)
     }
+    
     return render(request, 'expenses/index.html', context)
 
 
