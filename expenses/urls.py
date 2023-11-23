8e94b7759acffe88f8f2b06557dc838e0dbc2e0f
bigbugboy <liuxu-ynl@fixmail.com>
2023-11-23 13:23:43 +0800

    expense index summary stats

--- expenses/urls.py
+++ expenses/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('add-expense', views.add_expense, name='add-expense'),
     path('edit-expense/<id>', views.edit_expense, name='edit-expense'),
     path('delete-expense/<id>', views.delete_expense, name='delete-expense'),
+    path('expense-summary', views.expense_summary_index, name='expense_summary'),
+    path('this_month_data_stats', views.this_month_data_stats, name='this_month_data_stats'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -3,6 +3,10 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.paginator import Paginator
 from django.db.models import Q
+from django.http import JsonResponse
+import datetime
+from collections import defaultdict
+
 
 from .models import Category, Expense
 
@@ -22,7 +26,7 @@ def index(request):
             Q(category__icontains=search)
         )
 
-    paginator = Paginator(expenses, 2)
+    paginator = Paginator(expenses, 3)
     context = {
         'page_obj': paginator.get_page(page),
         'search_text': search
@@ -102,3 +106,31 @@ def delete_expense(request, id):
     Expense.objects.filter(pk=id).delete()
     messages.success(request, '删除成功')
     return redirect(to='expenses')
+
+
+@login_required(login_url='login')
+def this_month_data_stats(request):
+    start_date = datetime.date.today().replace(day=1)
+    expenses = Expense.objects.filter(owner=request.user, date__gte=start_date).all()
+
+    # amount per category
+    # amount per day
+    category_dict = defaultdict(int)
+    day_dict = defaultdict(int)
+    for item in expenses:
+        category_dict[item.category] += item.amount
+        day_dict[item.date] += item.amount
+    category_stats = [{'name': k, 'value': v} for k, v in category_dict.items()]
+    day_stats = [[str(k), v] for k, v in day_dict.items()]
+
+
+    response_data = {
+        'category_stats': category_stats,
+        'day_stats': sorted(day_stats, key=lambda x: x[0]),
+    }
+    return JsonResponse(response_data)
+
+
+
+def expense_summary_index(request):
+    return render(request, 'expenses/summary_index.html')
